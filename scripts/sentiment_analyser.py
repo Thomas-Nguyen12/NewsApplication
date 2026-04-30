337730a2fdf15749436cbafdaae953e32c28ddff
Thomas-Nguyen12 <thomas.nguyen3612@gmail.com>
2026-04-30 10:31:05 +0100

    I renamed the vinfast forecaster to vinfast_open_forecaster, moving it to a vinfast/ folder within models/time_series/. I will create separate models for high, low and volume and adjusted close so that I can create longer-term predictions

--- scripts/sentiment_analyser.py
+++ scripts/sentiment_analyser.py
@@ -5,6 +5,10 @@ import re
 import shap
 import os
 
+
+from nltk.corpus import stopwords
+import spacy
+
 # Instead of this:
 # model = joblib.load("models/news_topic_classifier/news_topic_classifier.pkl")
 
@@ -12,7 +16,19 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 model = joblib.load(os.path.join(BASE_DIR, "models/sentiment_analyser/sentiment_analyser.pkl"))
 vectoriser = joblib.load(os.path.join(BASE_DIR, "models/sentiment_analyser/sentiment_vectoriser.pkl"))
-                         
+
+
+stop_words = list(set(stopwords.words('english')))
+nlp = spacy.load("en_core_web_sm")
+def lemmatize(text):
+    doc = nlp(text)
+    # Turn it into tokens, ignoring the punctuation
+    tokens = [token for token in doc if not token.is_punct]
+    # Convert those tokens into lemmas, EXCEPT the pronouns, we'll keep those.
+    lemmas = [token.lemma_ if token.pos_ != 'PRON' else token.orth_ for token in tokens]
+    return lemmas
+
+
 
                          
 class analyser:
