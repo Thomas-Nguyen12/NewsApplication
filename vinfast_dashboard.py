c7736c3f6e7cfcdfdfbe93218930402c9a6f648c
Thomas-Nguyen12 <thomas.nguyen3612@gmail.com>
2026-07-09 15:00:56 +0100

    Removed Unnecessary buttons

--- vinfast_dashboard.py
+++ vinfast_dashboard.py
@@ -35,11 +35,7 @@ eod_key = st.secrets['fin_historical_data']
 # creating a sidebar to navigate between pages
 with st.sidebar: 
     # include a button that can st.switch_page() to the news article page
-    if st.button("Home"):
-        st.switch_page(f"vinfast_dashboard.py")
-
-    if st.button("Recent News Articles"):
-        st.switch_page(f"pages/news_page.py")
+    st.header("Navigation Bar")
 
 # day, month and year 
 #
