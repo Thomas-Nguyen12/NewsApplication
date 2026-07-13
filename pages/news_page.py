4b5a497b3f1512c1b45bb9fc6e87aeafba2cb569
Thomas-Nguyen12 <thomas.nguyen3612@gmail.com>
2026-07-13 10:03:27 +0100

    I updated the y axis label in pages/news_page.py

--- pages/news_page.py
+++ pages/news_page.py
@@ -153,7 +153,8 @@ grouped_news_sentiment = grouped_news_sentiment.reset_index()
 
 # I need to manually set the colours
 fig = px.bar(grouped_news_sentiment, x='publishedAt', y='sentiment_count', color='sentiment', barmode='group',
-    color_discrete_sequence=['orange', 'red', 'green'], title="Recent News Sentiment Frequency")
+    color_discrete_sequence=['orange', 'red', 'green'], title="Recent News Sentiment Frequency",
+    labels={'sentiment_count': 'Number of News Articles'})
 st.plotly_chart(fig)
 
 
